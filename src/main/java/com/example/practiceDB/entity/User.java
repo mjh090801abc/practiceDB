14d1caf2ce0ece69c90eb31ad9eec4a05dd90bbe
mjh090801abc <moonjunghoon090801@sopeuteuweeoui-MacBookPro.local>
2025-09-22 14:54:37 +0900

    User라는 entity를 만들고 DB에서 id가 자동으로 올라갈 수 있도록 코드 작성

---
+++ src/main/java/com/example/practiceDB/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.practiceDB.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+
+
+
+}
