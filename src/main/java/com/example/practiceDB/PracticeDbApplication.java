2d0046e8c8e44e861c019b52322715c74f4095ae
mjh090801abc <moonjunghoon090801@sopeuteuweeoui-MacBookPro.local>
2025-09-22 14:36:07 +0900

    git test

---
+++ src/main/java/com/example/practiceDB/PracticeDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.practiceDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeDbApplication.class, args);
+	}
+
+}
