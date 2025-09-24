211c2f5dc72897767b44e56d50cc31b5e80dd1e8
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-09-24 14:55:15 +0900

    findBy필드명을 활용해서 자동으로 쿼리를 만드는 코드 작성

--- src/main/java/com/example/practiceDB/repository/UserRepository.java
+++ src/main/java/com/example/practiceDB/repository/UserRepository.java
@@ -4,6 +4,6 @@ import com.example.practiceDB.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Integer, Long> {
-
+    User findByUsername(String username);
 
 }
