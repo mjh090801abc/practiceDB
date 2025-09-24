2caaff77dcace2b6276c13df243665968352eac5
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-09-24 14:53:44 +0900

    UserRepository가 JpaRepository에게 상속 받는 코드 작성

---
+++ src/main/java/com/example/practiceDB/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.practiceDB.repository;
+
+import com.example.practiceDB.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Integer, Long> {
+
+
+}
