1c16ad96a4617c85ed8743d5ea8ec37caa6132b0
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-09-26 12:28:13 +0900

    제네릭 타입을 반대로 써서 다시 수정

--- src/main/java/com/example/practiceDB/repository/UserRepository.java
+++ src/main/java/com/example/practiceDB/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.example.practiceDB.repository;
 import com.example.practiceDB.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Integer, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     User findByUsername(String username);
     boolean existsByUsername(String username);
 }
