355894dae94ce8a6c750999a636c5460d27ded9e
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-09-26 12:16:09 +0900

    userRepository 생성자를 주입

---
+++ src/main/java/com/example/practiceDB/service/UserService.java
@@ -0,0 +1,16 @@
+package com.example.practiceDB.service;
+
+import com.example.practiceDB.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+}
