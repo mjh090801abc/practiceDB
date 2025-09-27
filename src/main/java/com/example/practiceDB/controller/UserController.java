373eb870d88d0888825bbf4bf2861dfc2cd1ebdc
문정훈 <mjh090801abc@dgsw.hs.kr>
2025-09-27 15:45:44 +0900

    회원가입 html로 넘어갈 수 있게 getmapping 해주기

--- src/main/java/com/example/practiceDB/controller/UserController.java
+++ src/main/java/com/example/practiceDB/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.practiceDB.controller;
 
+import com.example.practiceDB.entity.User;
 import com.example.practiceDB.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,5 +14,11 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
 
+    }
+
+    @GetMapping("/signup")
+    public String signupForm(Model model) {
+        model.addAttribute("user", new User());
+        return "signup";
     }
 }
